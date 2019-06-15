e7e8d4e9810f0e8998d4497e7942cb0537dc696e
Karishma Patel <patelkarishma10@gmail.com>
2019-06-15 16:19:58 +0100

    first created this files on the backend repo but have now moved them to this repo

---
+++ index.js
@@ -0,0 +1,50 @@
+function makeRequest(method, url, body) {
+    return new Promise(
+        function (resolve, reject) {
+            let req = new XMLHttpRequest();
+
+            req.onload = function () {
+                const data = JSON.parse(req.responseText);
+                if (req.status >= 200 && req.status < 300) {
+                    resolve(data);
+                } else {
+                    const reason = new Error('Rejected');
+                    reject(reason);
+                }
+            };
+
+            req.open(method, url);
+            req.send(JSON.stringify(body));
+        }
+    );
+}
+
+function getAUser() {
+    let id = Number(document.getElementById("anAcc").value);
+    makeRequest("GET", `http://localhost:8080/SoloProject/api/user/getAUser/${id}`)
+        .then((data) => {
+            const username = data.username;
+            const email = data.email;
+            document.getElementById("accountUsername").innerHTML = username;
+            document.getElementById("accountEmail").innerHTML = email;
+            console.log(data);
+        })
+        .catch((error) => console.log(error.message));
+    return false;
+}
+
+
+function createAccount() {
+    let newAcc = {
+        username: document.getElementById("accUsername").value,
+        password: document.getElementById("accPassword").value,
+        email: document.getElementById("accEmail").value
+    };
+    makeRequest("POST", "http://localhost:8080/SoloProject/api/user/createUser", newAcc)
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => console.log(error.message));
+
+    return false;
+}
